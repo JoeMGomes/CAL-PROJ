be286dda38f200fcce75f0af1f91c30457ed7430
José Gomes <zemiguelgomes99@gmail.com>
2019-05-19 23:07:49 +0100

    CAL e feio

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,60 @@
 #include <sstream>
 #include <Windows.h>
 
-
-
-
-
-void inicia()
-{
+void inicia() {
 	GraphViewer *gv = new GraphViewer(600, 600, false);
-	gv->createWindow(600, 600);
+	gv->createWindow(800, 800);
 	gv->defineVertexColor("blue");
 	gv->defineEdgeColor("black");
-	ifstream f;
-	f.open("nos.txt");
-	if (!f) exit(1);
-	int id,x,y;
-	char c;
-	while(!f.eof()){
-		f>>id;f>>c;f>>x;f>>c;f>>y;
-		gv->addNode(id,x,y);
-	}
-	f.close();
 
-	f.open("arestas.txt");
-	if (!f) exit(1);
-	while(!f.eof()){
-		f>>id;f>>c;f>>x;f>>c;f>>y;
-		gv->addEdge(id,x,y,EdgeType::UNDIRECTED);
+	gv->addNode(1, 1, 2);
+
+	ifstream file;
+	file.open("testeNodes.txt");
+	//f.open("nos.txt");
+
+	if (!file) {
+		cout << "Failed to open node file\n";
+		exit(1);
+	}
+
+	string line;
+	int id, numNos;
+	double x, y;
+	getline(file,line);
+	sscanf(line.c_str(), "%d",&numNos);
+
+	while (getline(file,line) && numNos != 0) {
+		cout << numNos << endl;
+		sscanf(line.c_str(),"(%d, %lf, %lf)", &id, &x,&y);
+		gv->addNode(id, x, y);
+		numNos--;
+	}
+	cout << "Completed reading " << numNos << "nodes\n";
+	file.close();
+
+	//gv->rearrange();
+
+	/*f.open("arestas.txt");
+
+	if (!f){
+		cout << "Failed to open edge file\n";
+		exit(1);
+	}
+
+	f >> numNos;
+	cout << numNos << endl;
+	while (!f.eof()) {
+		f >> c;
+		f >> x;
+		f >> c;
+		f >> y;
+		f >> c;
+		gv->addEdge(id, x, y, EdgeType::UNDIRECTED);
 	}
 	f.close();
+	cout << "Completed edge reading\n";*/
 }
-
 
 int main() {
 	inicia();
