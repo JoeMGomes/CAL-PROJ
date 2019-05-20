7262748430855eb1d3b2d60290ddaff79d91360c
jfcaraujo <jfcaraujo99@gmail.com>
2019-05-20 15:22:01 +0100

    Increased board size

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#ifdef linux
+#else
 #include <Windows.h>
+#endif
 
 void inicia() {
 	GraphViewer *gv = new GraphViewer(600, 600, false);
-	gv->createWindow(800, 800);
+	gv->createWindow(80000, 80000);
 	gv->defineVertexColor("blue");
 	gv->defineEdgeColor("black");
 
-	gv->addNode(1, 1, 2);
-
+	gv->addNode(1, 1, 1);
+	gv->addNode(2, 1000, 1000);
+	gv->addNode(3, 10000, 10000);
+	gv->addNode(4, 100000, 100000);
 	ifstream file;
-	file.open("testeNodes.txt");
+	file.open("T09/Porto/T09_nodes_X_Y_Porto.txt");
 	//f.open("nos.txt");
 
 	if (!file) {
@@ -23,18 +28,18 @@ void inicia() {
 	}
 
 	string line;
-	int id, numNos;
-	double x, y;
+	int numNos;
+	double id, x, y;
 	getline(file,line);
 	sscanf(line.c_str(), "%d",&numNos);
 
 	while (getline(file,line) && numNos != 0) {
 		cout << numNos << endl;
-		sscanf(line.c_str(),"(%d, %lf, %lf)", &id, &x,&y);
+		sscanf(line.c_str(),"(%lf, %lf, %lf)", &id, &x,&y);
 		gv->addNode(id, x, y);
 		numNos--;
 	}
-	cout << "Completed reading " << numNos << "nodes\n";
+	cout << "Completed reading " << numNos << "nodes\n";//vai dar sempre 0, é estupido
 	file.close();
 
 	//gv->rearrange();
